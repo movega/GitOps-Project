117376571a62bc85edf05810b184f6d6c8aa2fe5
movega <alvarovegamotrel2@gmail.com>
2026-04-28 13:17:50 +0200

    Clean frontend

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,29 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
-const isTest = process.env.VITEST === 'true'
-
 export default defineConfig({
   build: {
     sourcemap: 'hidden',
   },
-  test: {
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-    include: ['src/**/*.test.ts', 'src/**/*.test.tsx'],
-  },
-  plugins: [
-    react({
-      babel: isTest
-        ? undefined
-        : {
-            plugins: [
-              'react-dev-locator',
-            ],
-          },
-    }),
-    tsconfigPaths()
-  ].filter(Boolean),
+  plugins: [react()],
 })
