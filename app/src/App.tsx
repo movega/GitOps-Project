de264cb55cd8f97c449be24f39df9dd2a92b1417
movega <alvarovegamotrel2@gmail.com>
2026-04-29 13:40:43 +0200

    Change name for web app

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-4xl font-semibold">Hola Alvaro</h1>
+      <h1 className="text-4xl font-semibold">Hola Mundo</h1>
     </main>
   );
 }
